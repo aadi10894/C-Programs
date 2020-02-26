62f19d687c9950034efaf9cade84491aec778e7a
aadi10894 <55064617+aadi10894@users.noreply.github.com>
2020-02-26 00:34:15 -0600

    Add files via upload

---
+++ Perfect Number/prog2.h
@@ -0,0 +1,17 @@
+#ifndef H_PROG2
+
+#define H_PROG2
+
+#include<iostream> // Library to access input and output operations
+
+#include<string> // Library to access strings in the program
+
+using namespace std;
+
+bool isPerfect(const unsigned int n); // We are function prototyping here as in we are declaring the function in the header file which has it definition in prog2.cc
+
+void divisors(const unsigned int divisor);
+
+const unsigned int finalValue = 9999; // As per the instruction provided in assignment I have defined the constant value in the header file.
+
+#endif
