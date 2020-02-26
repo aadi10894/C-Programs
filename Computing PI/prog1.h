540bac6ab6847688997d42c9b7ec6a15094be692
aadi10894 <55064617+aadi10894@users.noreply.github.com>
2020-02-26 00:29:24 -0600

    Add files via upload

---
+++ Computing PI/prog1.h
@@ -0,0 +1,6 @@
+#ifndef H_PROG1
+#define H_PROG1
+#include <iostream> //library for input output operations
+using namespace std; //header file to include features of c++ standard library
+double add_terms(int val); //function prototyping
+#endif
